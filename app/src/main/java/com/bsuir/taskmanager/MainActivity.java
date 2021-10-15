7ffcfd9714816d80adf14024cc4807609f371402
Potass1umN1trate <anonymous98743@protonmail.com>
2021-10-16 01:19:48 +0300

    Adding Task's Creation Module

--- app/src/main/java/com/bsuir/taskmanager/MainActivity.java
+++ app/src/main/java/com/bsuir/taskmanager/MainActivity.java
@@ -1,76 +1,46 @@
-package com.bsuir.taskmanager;
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.GridLayout;
 
-import com.google.android.material.snackbar.Snackbar;
+public class MainActivity extends Activity {
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.bsuir.taskmanager.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    public static String TASK_NAME = "taskname";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(TASK_NAME);
+        if(text!=null) {
+            RelativeLayout rl = (RelativeLayout) findViewById(R.id.layout);
+            ListView listView = new ListView(this);
+            listView.setLayoutParams(new ViewGroup.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT));
+            Task task = new Task(this, text);
+            ArrayAdapter<String> taskList = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    Task.getTasks());
+            listView.setAdapter(taskList);
+            rl.addView(listView);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void goToCreationActivity(View view){
+        Intent creationActivity = new Intent(this, CreationActivity.class);
+        startActivity(creationActivity);
     }
 }
