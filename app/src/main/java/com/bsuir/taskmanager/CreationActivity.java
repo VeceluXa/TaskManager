c1cd28e5a40efd2275c77d58cca0fc765857f92a
Potass1umN1trate <anonymous98743@protonmail.com>
2021-10-17 02:23:50 +0300

    Adding creation tasks module

---
+++ app/src/main/java/com/bsuir/taskmanager/CreationActivity.java
@@ -0,0 +1,14 @@
+package com.bsuir.taskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreationActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_acticity);
+    }
+}
