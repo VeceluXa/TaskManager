19a6d615f5648430cfd29011bde2ce4700103495
Fyodor Danilov <62480297+VeceluXa@users.noreply.github.com>
2021-10-17 13:10:01 +0300

    Revert "Updating task creation module"
    This reverts commit cea6b2b33468f6f3e86d3b2d2eb60b5a2b39a872.

--- app/src/main/java/com/bsuir/taskmanager/CreationActivity.java
+++ app/src/main/java/com/bsuir/taskmanager/CreationActivity.java
@@ -1,49 +1,14 @@
 package com.bsuir.taskmanager;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-
-public class CreationActivity extends Activity{
-
-    private EditText task;
-    private SubtaskAdapter adapter = null;
-    private RecyclerView rv;
+public class CreationActicity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_creation);
-
-        task = (EditText) findViewById(R.id.taskTODO);
-        task.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean hasFocus) {
-                if(!hasFocus){
-                    if(adapter == null){
-                        adapter = new SubtaskAdapter();
-                        rv.setAdapter(adapter);
-                    }
-                }
-            }
-        });
-        rv = (RecyclerView) findViewById(R.id.subtasksList);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-
-
-    public void saveTask(View view){
-
+        setContentView(R.layout.activity_creation_acticity);
     }
 }
