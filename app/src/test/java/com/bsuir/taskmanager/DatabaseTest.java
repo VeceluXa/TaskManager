23578ba4d529c37c0a79a7915b1b490da33d9a36
Fyodor Danilov <62480297+VeceluXa@users.noreply.github.com>
2021-11-15 22:41:21 +0300

    Added jacoco configuration to gradle scripts

--- app/src/test/java/com/bsuir/taskmanager/DatabaseTest.java
+++ app/src/test/java/com/bsuir/taskmanager/DatabaseTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertEquals;
 import junit.framework.TestCase;
 
 public class DatabaseTest extends TestCase {
-
+    /**
+     * First Unit Test
+     */
     public void testAddition() {
         Database database = new Database();
         assertEquals(database.addition(2, 2), 2 + 2);
